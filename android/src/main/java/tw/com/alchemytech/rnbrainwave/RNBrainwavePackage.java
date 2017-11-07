782d9184e88072bb63a7e14b32a4d0f05f8a1c29
jimmy <jimmy@meego.tw>
2017-11-07 11:27:41 +0800

    remove override

--- android/src/main/java/tw/com/alchemytech/rnbrainwave/RNBrainwavePackage.java
+++ android/src/main/java/tw/com/alchemytech/rnbrainwave/RNBrainwavePackage.java
@@ -19,7 +19,6 @@ public class RNBrainwavePackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
